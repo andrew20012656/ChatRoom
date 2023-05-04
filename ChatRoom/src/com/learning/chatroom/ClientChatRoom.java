2e711c56f80f57c7f8a23d9fe7c87e9be1df7def
andrew20012656 <andrew20012656@gmail.com>
2023-05-04 14:51:37 -0400

    implemented multi-client chatroom

--- ChatRoom/src/com/learning/chatroom/ClientChatRoom.java
+++ ChatRoom/src/com/learning/chatroom/ClientChatRoom.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
@@ -12,9 +13,16 @@ public class ClientChatRoom extends JFrame {
     private static final String CONNECTION = "127.0.0.1";
     private static final int CONNECTION_PORT = 8888;
     private JTextArea textArea = new JTextArea(10, 20);
+    private JScrollPane scrollPane = new JScrollPane(textArea);
     private JTextField textField = new JTextField(20);
     private Socket socket = null;
+    private boolean isConnected = false;
     private DataOutputStream dos = null;
+
+    public static void main(String[] args) {
+        ClientChatRoom cc1 = new ClientChatRoom();
+        cc1.init();
+    }
 
     public ClientChatRoom() throws HeadlessException {
         super();
@@ -22,7 +30,7 @@ public class ClientChatRoom extends JFrame {
 
     public void init() {
         this.setTitle("Client Chatroom");
-        this.add(textArea, BorderLayout.CENTER);
+        this.add(scrollPane, BorderLayout.CENTER);
         this.add(textField, BorderLayout.SOUTH);
         this.setBounds(300, 300, 300, 400);
 
@@ -30,13 +38,11 @@ public class ClientChatRoom extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String messageToSend = textField.getText();
-                send(messageToSend);
-
                 if(messageToSend.trim().length() == 0) {
                     return;
                 }
+                send(messageToSend);
                 textField.setText("");
-                textArea.append(messageToSend + "\n");
             }
         });
 
@@ -46,10 +52,13 @@ public class ClientChatRoom extends JFrame {
 
         try {
             socket = new Socket(CONNECTION, CONNECTION_PORT);
+            isConnected = true;
         } catch (IOException e) {
             e.printStackTrace();
         }
         this.setVisible(true);
+
+        new Thread(new ReceiveMessage()).start();
     }
 
     //
@@ -60,11 +69,23 @@ public class ClientChatRoom extends JFrame {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    public static void main(String[] args) {
-        ClientChatRoom cc1 = new ClientChatRoom();
-        cc1.init();
+    class ReceiveMessage implements Runnable {
+        @Override
+        public void run() {
+            while(isConnected) {
+                try {
+                    DataInputStream dataIn = new DataInputStream(socket.getInputStream());
+                    String messageFromServer = dataIn.readUTF();
+                    textArea.append(messageFromServer + "\n");
+                } catch (IOException e) {
+                    System.out.println("The server is disconnected.");
+                    textArea.append("The server is disconnected.\n");
+                    isConnected = false;
+                }
+            }
+        }
     }
+
 }
--- ChatRoom/src/com/learning/chatroom/Server.java
+++ ChatRoom/src/com/learning/chatroom/Server.java
@@ -4,21 +4,74 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Server extends JFrame {
     private static final int PORT = 8888;
     private JTextArea serverTextArea = new JTextArea();
+    private JScrollPane scrollPane = new JScrollPane(serverTextArea);
     private JPanel buttonTool = new JPanel();
     private JButton startButton = new JButton("Launch");
     private JButton stopButton = new JButton("Stop");
 
     private ServerSocket serverSocket = null;
     private Socket socket = null;
+    private ArrayList<ClientConnection> connectionsList = new ArrayList<>();
+
     private DataInputStream dis = null;
+    private boolean isLaunched = false;
+
+    class ClientConnection implements Runnable{
+        Socket socket = null;
+
+        public ClientConnection(Socket socket) {
+            this.socket = socket;
+            (new Thread(this)).start();
+        }
+
+        // Simultaneously receive messages from all clients
+        @Override
+        public void run() {
+            try {
+                DataInputStream dataIn = new DataInputStream(socket.getInputStream());
+                // The server can receive all messages sent by each client from each connection
+                while(isLaunched) {
+                    String messageReceived = dataIn.readUTF();
+                    System.out.println("Server received the message: " + messageReceived);
+                    String clientIPAndPort = socket.getInetAddress() + "|" + socket.getPort();
+                    String messageToSend = clientIPAndPort + ": " + messageReceived + "\n";
+                    serverTextArea.append(messageToSend); // show the message on Server side
+
+                    Iterator<ClientConnection> iterator = connectionsList.iterator();
+                    while(iterator.hasNext()) {
+                        ClientConnection connection = iterator.next();
+                        connection.send(messageToSend);
+                    }
+                }
+            } catch (IOException e) {
+//                e.printStackTrace();
+                System.out.println("The client has disconneted.");
+                serverTextArea.append(socket.getInetAddress() + "|" + socket.getPort() + " is disconnected");
+            }
+        }
+
+        public void send(String message) {
+            try {
+                DataOutputStream dataOut = new DataOutputStream(this.socket.getOutputStream());
+                dataOut.writeUTF(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Server server = new Server();
@@ -26,47 +79,93 @@ public class Server extends JFrame {
 
     public Server() throws HeadlessException {
         this.setTitle("Server log");
-        this.add(serverTextArea, BorderLayout.CENTER);
+        this.add(scrollPane, BorderLayout.CENTER);
         buttonTool.add(startButton);
         buttonTool.add(stopButton);
         this.add(buttonTool, BorderLayout.SOUTH);
 
         this.setBounds(0, 0, 500, 500);
-        serverTextArea.append("Server hasn't launched yet. Please launch the server.\n");
+
+        if(isLaunched) {
+            serverTextArea.append("Server is running.\n");
+        } else {
+            serverTextArea.append("Server hasn't launched yet. Please launch the server.\n");
+        }
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                isLaunched = false;
+                try {
+                    if(serverSocket != null) serverSocket.close();
+                    serverTextArea.append("The server is disconnected.");
+                    System.exit(0);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        stopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if(serverSocket != null) {
+                        serverSocket.close();
+                        isLaunched = false;
+                    }
+                    serverTextArea.append("The server is disconnected.");
+                    System.exit(0);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
 
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                startServer();
+                try{
+                    if(serverSocket == null) {
+                        serverSocket = new ServerSocket(PORT);
+                    }
+                    isLaunched = true;
+                    System.out.println("The server has launched");
+                    serverTextArea.append("The server has launched \n");
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             }
         });
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Server should shutdown when closed window -- need improvement
+
         serverTextArea.setEditable(false);
         this.setVisible(true);
+        startServer();
     }
 
     public void startServer() {
         try {
-            serverSocket = new ServerSocket(PORT);
-            System.out.println("The server has launched");
-            socket = serverSocket.accept();
-            System.out.println("A client has connected to the server: " + socket.getInetAddress() + "/" + socket.getPort());
-            serverTextArea.append("A client has connected to the server: " + socket.getInetAddress() + "/" +
-                    socket.getPort() + "\n");
+            try {
+                serverSocket = new ServerSocket(PORT);
+                isLaunched = true;
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
 
-            receiveMessage();
+            // connecting to multiple clients
+            while (isLaunched) {
+                System.out.println("always true");
+                Socket newSocket = serverSocket.accept();
+                connectionsList.add(new ClientConnection(newSocket));
+                System.out.println("A client has connected to the server: " + newSocket.getInetAddress() + "/" + newSocket.getPort());
+                serverTextArea.append("A client has connected to the server: " + newSocket.getInetAddress() + "/" +
+                        newSocket.getPort() + "\n");
+            }
         } catch (IOException e) {
         }
     }
 
-    public void receiveMessage() {
-        try {
-            dis = new DataInputStream(socket.getInputStream());
-            String message = dis.readUTF();
-            System.out.println(message);
-            serverTextArea.append(message);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void stopServer() {
+
     }
 }
